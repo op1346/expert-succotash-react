1ce6fd87b6a060e188cd321a4f14559ef59b274a
Olivia Park <olivia.park1346@gmail.com>
2020-12-09 08:39:51 +0900

    change home to homegoods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 
 import NavBar from "./NavBar";
 import Main from "./Main";
-import Home from "./Home.jsx";
+import Homegoods from "./Homegoods.jsx";
 import Clothes from "./Clothes.jsx";
 import Food from "./Food.jsx";
 
@@ -14,7 +14,7 @@ const App = () => {
     <HashRouter>
         <Route path="/" component={NavBar}/>
       <Switch>
-        <Route path="/home" component={Home}/>
+        <Route path="/homegoods" component={Homegoods}/>
         <Route path="/clothes" component={Clothes}/>
         <Route path="/food" component={Food}/>
         <Route path="/" component={Main} />
---
+++ src/components/Homegoods.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homegoods() {
+  return(
+    <div>
+      <h1>Homegoods</h1>
+    </div>
+  )
+};
+
+export default Homegoods
