b08c63f775afe7388ee1629c1e80e71b8953b131
jenlu33 <nejul33@gmail.com>
2020-12-08 12:06:59 -0500

    Fix rendering issue

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css';
+import Home from "./Home.jsx"
+
+const App = () => {
+  return(
+    <div>
+      <h1>Help me find sustainable...</h1>
+      <Home />
+    </div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App.jsx';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<App />, root);
+})
 
 
