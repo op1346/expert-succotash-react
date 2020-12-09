45b5771cbc0806d8a06819d9bf9040e1a2ed4f90
jenlu33 <nejul33@gmail.com>
2020-12-08 22:52:57 -0500

    Add data skeleton and example in Food component

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import data from "../data/data";
+
 function Food() {
   return(
     <div>
       <h1>Food</h1>
+      {data.map(item => (
+        item.category === "food" && <div>{item.companyName}</div>
+      ))}
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import data from "../data/data";
+
 function Main() {
   return(
     <div className="main-container">
       <h1>ECO SHOP</h1>  
       {/* ^ placeholder name. Idk */}
       <h4>What is eco shopping?</h4>
+      
     </div>
   )
 };
