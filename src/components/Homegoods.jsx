45d71ad51f48c6dd5ca783cbda8727d36e27aa88
Olivia Park <olivia.park1346@gmail.com>
2020-12-09 21:06:40 +0900

    add links and company_info.jsx

--- src/components/Homegoods.jsx
+++ src/components/Homegoods.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import data from "../data/data";
+
 function Homegoods() {
   return(
     <div className="main-container">
       <h1>Homegoods</h1>
+
+      {data.map(item => (
+        item.category === "homegoods" &&
+        <div>
+          <a href={item.url}>{item.companyName}</a>
+          <p>{item.desc}</p>
+        </div>
+      ))}
     </div>
   )
 };
--- src/data/data.js
+++ src/data/data.js
@@ -9,9 +9,51 @@ const data = [
     {
       companyName: "Pact",
       category: "clothing",
-      desc: "",
+      desc: "fdafdsa",
       subCategories: [],
       url: "https://wearpact.com"
+    },
+    {
+      companyName: "Thought",
+      category: "clothing",
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://www.wearethought.com/"
+    },
+    {
+      companyName: "Organic Basics",
+      category: "clothing",
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://us.organicbasics.com/"
+    },
+    {
+      companyName: "Outer Known",
+      category: "clothing",
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://www.outerknown.com/"
+    },
+    {
+      companyName: "Alternative Apparel",
+      category: "clothing",
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://www.alternativeapparel.com/"
+    },
+    {
+      companyName: "Eco Collective",
+      category: ["homegoods", "beauty"],
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://www.ecocollective.com/"
+    },
+    {
+      companyName: "Naadam",
+      category: "clothing",
+      desc: "fdafdsa",
+      subCategories: [],
+      url: "https://naadam.co/"
     },
   ];
 
