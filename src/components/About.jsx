0f3afba3031fc42f331ff88c9fb8127aa59c4b22
Olivia Park <olivia.park1346@gmail.com>
2020-12-10 18:53:50 +0900

    color change to main-container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return(
     <div className="main-container">
       <h1>About Us</h1>
-      <h3>Just two kids excited and worried about our planet at the same time. Help us help our planet by making different decisions about the products that we buy. Start supporting local and sustainability!</h3>
+      <h3>Just two kids excited and worried about our planet at the same time.<br />Help us help our planet by making different decisions about the products that we buy. Start supporting local and sustainability!</h3>
 
     </div>
   )
