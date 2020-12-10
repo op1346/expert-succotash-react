2bc9c0aaededb1c5d738d8b37bb36630c2744b3c
Olivia Park <olivia.park1346@gmail.com>
2020-12-10 20:51:04 +0900

    Update to Main.jsx and add resourceData.js

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return(
     <div className="main-container">
       <h1>About Us</h1>
-      <h3>Just two kids excited and worried about our planet at the same time.<br />Help us help our planet by making different decisions about the products that we buy. Start supporting local and sustainability!</h3>
+      <h3>Just two kids excited and worried about our planet at the same time.<br />Help us help our planet by making different decisions about the products that we buy. <br />Start supporting local and sustainability!</h3>
 
       <p>We're also looking for jobs at the moment so check out our portfolios.</p>
       <p><a href="http://hellojenlu.com/">Jen's Portfolio Website</a></p>
