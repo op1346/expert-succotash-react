0006ebb2bdab9feb1eb3efb5c6f3468eeba09dea
op1346 <olivia.park1346@gmail.com>
2021-01-18 20:15:48 +0900

    Change file structures for better organization

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { setUserSession } from '../Utils/Common';
-import SignUp from '../components/SignUp';
+import SignUp from '../SignUp';
 
 function Login(props) {
   const email = useFormInput('');
